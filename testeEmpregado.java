b638d9d5e2867f01b446953a3b111e4020f7feeb
Andre <andreg.olveira@gmail.com>
2023-04-08 22:35:16 -0300

    Terceiro Commit. Adicionado teste

---
+++ testeEmpregado.java
@@ -0,0 +1,8 @@
+public class testeEmpregado {
+    public static void main (String [] args){
+        Empregada e1 = new Empregada ();
+        Empregada e2 = new Empregada();
+        Empregada e3 = new Empregada ();
+        //e agora, como configurar o tipo deles?
+        }
+}
